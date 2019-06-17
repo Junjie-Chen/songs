49556c2282726fe3ccd8adbeaede3818e1792a7d
Junjie Chen <junjie.chen18@gmail.com>
2019-06-17 19:06:17 -0400

    feat(src): pass selectedSong prop to SongDetail component

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = () => {
-  return <div>Song Detail</div>;
+const SongDetail = props => {
+  return <div>{props.selectedSong.title}</div>;
 };
 
 const mapStateToProps = state => ({
