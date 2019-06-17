e9fcffa5a315d145f968b83eb0e490163d4e8de8
Junjie Chen <junjie.chen18@gmail.com>
2019-06-17 19:18:37 -0400

    feat(src): render a selected song conditionally

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SongDetail = props => {
+  if (!props.selectedSong) {
+    return <div>Select a Song</div>;
+  }
+
   return <div>{props.selectedSong.title}</div>;
 };
 
